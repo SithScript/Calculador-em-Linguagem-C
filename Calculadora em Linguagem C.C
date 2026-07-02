7fee79faad9a71e46523358e26a372615d07064c
Jr. <leofredo.junior@gmail.com>
2026-07-01 22:44:47 -0300

    Atualizar o Calculadora em Linguagem C.C

--- Calculadora em Linguagem C.C
+++ Calculadora em Linguagem C.C
@@ -1,45 +1,52 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-float num1, num2;
-char op, stop;
+    float num1, num2;
+    char op, stop = 's';
 
-while(stop != 'n'){
-	
-	printf("Digite o primeiro numero: ");
-		scanf("%f", &num1);
-	
-	printf("Digite o segundo numero: ");
-			scanf("%f", &num2);
-		
-	printf("Digite a operacao desejada (/, +, -, *)\t" );		
-				scanf(" %c", &op);
-		
-switch (op){
-	
-case '/':
-	printf("Resultado: %.6f\n", num1 / num2);
-			break;
-			
-case '+':
-	printf("Resultado: %.6f\n", num1 + num2);
-			break;
-	
-	printf("Resultado: %.6f\n", num1 - num2);
-			break;
-	
-case '*':
-	printf("Resultado: %.6f\n", num1 * num2);
-			break;						
-	}
-	printf("Deseja fazer uma nova operacao? (s/n)\t");
-			scanf(" %c", &stop);
-	}
+    while (stop != 'n') {
 
-return 0;
+        printf("Digite o primeiro numero: ");
+        scanf("%f", &num1);
 
+        printf("Digite o segundo numero: ");
+        scanf("%f", &num2);
+
+        printf("Digite a operacao desejada (+, -, *, /): ");
+        scanf(" %c", &op);
+
+        switch (op) {
+
+            case '+':
+                printf("Resultado: %.6f\n", num1 + num2);
+                break;
+
+            case '-':
+                printf("Resultado: %.6f\n", num1 - num2);
+                break;
+
+            case '*':
+                printf("Resultado: %.6f\n", num1 * num2);
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    printf("Erro: divisao por zero!\n");
+                } else {
+                    printf("Resultado: %.6f\n", num1 / num2);
+                }
+                break;
+
+            default:
+                printf("Operacao invalida!\n");
+        }
+
+        printf("Deseja fazer uma nova operacao? (s/n): ");
+        scanf(" %c", &stop);
+    }
+
+    printf("Programa encerrado.\n");
+
+    return 0;
 }
-
-
-
